3006a6401fb962f942367706ca6459cada75ec0e
Fabio Barbato <57710212+Fabio-Barbato@users.noreply.github.com>
2021-04-19 19:42:09 +0200

    Add files via upload

--- LASD/exercise2/queue/queue.hpp
+++ LASD/exercise2/queue/queue.hpp
@@ -13,7 +13,7 @@ namespace lasd {
 /* ************************************************************************** */
 
 template <typename Data>
-class Queue { // Must extend Container
+class Queue: virtual public Container { // Must extend Container
 
 private:
 
@@ -26,31 +26,31 @@ protected:
 public:
 
   // Destructor
-  // ~Queue() specifiers
+  virtual ~Queue() = default;
 
   /* ************************************************************************ */
 
   // Copy assignment
-  // type operator=(argument); // Copy assignment of abstract types should not be possible.
+  Queue& operator=(const Queue<Data>&) = delete; // Copy assignment of abstract types should not be possible.
 
   // Move assignment
-  // type operator=(argument); // Move assignment of abstract types should not be possible.
+  Queue& operator=(Queue<Data>&&) noexcept = delete; // Move assignment of abstract types should not be possible.
 
   /* ************************************************************************ */
 
   // Comparison operators
-  // type operator==(argument) specifiers; // Comparison of abstract types might not be possible.
-  // type operator!=(argument) specifiers; // Comparison of abstract types might not be possible.
+  bool operator==(const Queue<Data>&) const noexcept = delete; // Comparison of abstract types might not be possible.
+  bool operator!=(const Queue<Data>&) const noexcept = delete; // Comparison of abstract types might not be possible.
 
   /* ************************************************************************ */
 
   // Specific member functions
 
-  // type Enqueue(argument) specifiers; // Copy of the value
-  // type Enqueue(argument) specifiers; // Move of the value
-  // type Head() specifiers; // (concrete function must throw std::length_error when empty)
-  // type Dequeue() specifiers; // (concrete function must throw std::length_error when empty)
-  // type HeadNDequeue() specifiers; // (concrete function must throw std::length_error when empty)
+  void Enqueue(const Data&); // Copy of the value
+  void Enqueue(Data&&) noexcept; // Move of the value
+  Data& Head() const; // (concrete function must throw std::length_error when empty)
+  void Dequeue(); // (concrete function must throw std::length_error when empty)
+  Data HeadNDequeue(); // (concrete function must throw std::length_error when empty)
 
 };
 
