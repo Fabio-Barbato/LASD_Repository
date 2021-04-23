d7d6f7d2c31686d615b8b717af314b6b52396c7c
Fabio Barbato <57710212+Fabio-Barbato@users.noreply.github.com>
2021-04-23 12:00:47 +0200

    quevec.cpp
    Scritto metodo Enqueue per copy;

--- LASD/exercise2/queue/vec/queuevec.cpp
+++ LASD/exercise2/queue/vec/queuevec.cpp
@@ -3,7 +3,36 @@ namespace lasd {
 
 /* ************************************************************************** */
 
-// ...
+//Enqueue (copy)
+template <typename Data>
+void QueueVec<Data>::Enqueue(const Data& value){
+  if(size==0){ //Queue vuota
+    Elements = new Data[10];
+    size = 10;
+    Elements[0] = value;
+    head = &Elements[0];
+    tail = &Elements[1];
+  }
+  else{ //Queue non piena
+    Elements[tail] = value;
+    if (tail+1 > &Elements[size-1]){ //tail arrivata alla fine del Vector
+      if(head == &Elements[0]){ //Vector pieno
+        Expand();
+      }
+      else{
+        tail = Elements[0];
+      }
+    }
+    else{//tail non alla fine del Vector
+      if(head == tail+1){//Vector pieno
+        Expand();
+      }
+      else{
+        tail++;
+      }
+    }
+  }
+ }
 
 /* ************************************************************************** */
 
