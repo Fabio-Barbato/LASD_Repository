593cd9149bd9c5490c5ac140fcce1b52e02573e1
Fabio Barbato <57710212+Fabio-Barbato@users.noreply.github.com>
2021-05-30 22:58:18 +0200

    matrixvec
    completato matrixvec!

--- LASD/exercise5/matrix/vec/matrixvec.cpp
+++ LASD/exercise5/matrix/vec/matrixvec.cpp
@@ -50,10 +50,11 @@ MatrixVec<Data>& MatrixVec<Data>::operator=(const MatrixVec<Data>& mat){
 
 // Move assignment
 template <typename Data>
-MatrixVec<Data>& MatrixVec<Data>::operator=(MatrixVec<Data>&& mat) noexcept: vec(std::move(mat.vec)){
+MatrixVec<Data>& MatrixVec<Data>::operator=(MatrixVec<Data>&& mat) noexcept{
   std::swap(rows , mat.rows);
   std::swap(columns , mat.columns);
   std::swap(size , mat.size);
+  vec = std::move(mat.vec);
   return *this;
 }
 
@@ -74,37 +75,103 @@ bool MatrixVec<Data>::operator!=(const MatrixVec<Data>& mat) const noexcept{
 
 // Specific member functions (inherited from Matrix)
 
-void RowResize(const ulong) override; // Override Matrix member
-void ColumnResize(const ulong) override; // Override Matrix member
 
-bool ExistsCell(const ulong, const ulong) noexcept; // Override Matrix member (should not throw exceptions)
+template <typename Data>
+void MatrixVec<Data>::RowResize(const ulong new_rows) {
+  rows = new_rows;
+  size = new_rows*columns;
+  vec.Resize(size);
+}
 
-Data& operator()(const ulong, const ulong); // Override Matrix member (mutable access to the element; throw out_of_range when out of range)
-const Data& operator()(const ulong, const ulong) const; // Override Matrix member (immutable access to the element; throw out_of_range when out of range and length_error when not present)
+template <typename Data>
+void MatrixVec<Data>::ColumnResize(const ulong new_col) {
 
+  if(vec.Size()>0){
+    Vector<Data> tmp(new_col*rows);
+    if(new_col<columns){
+      for (ulong i = 0; i < rows; i++) {
+        for (ulong j = 0; j < new_col; j++) {
+          tmp[i*new_col+j] = vec[i*columns+j];
+        }
+      }
+      vec = std::move(tmp);
+    }
+    else{
+      for (ulong i = 0; i < rows; i++) {
+        for (ulong j = 0; j < columns; j++) {
+          tmp[i*new_col+j] = vec[i*columns+j];
+        }
+      }
+      vec = std::move(tmp);
+    }
+
+  }
+  else
+    vec.Resize(new_col*rows);
+
+    columns = new_col;
+    size = rows*columns;
+
+
+}
+
+
+template <typename Data>
+bool MatrixVec<Data>::ExistsCell(const ulong row, const ulong col) const noexcept{
+  return (size>0 && row<=rows && col<=columns);
+}
+
+
+template <typename Data>
+Data& MatrixVec<Data>::operator()(const ulong row, const ulong col){
+  return const_cast<Data&>(static_cast<const MatrixVec<Data>*>(this)->operator()(row, col));
+}
+
+
+template <typename Data>
+const Data& MatrixVec<Data>::operator()(const ulong row, const ulong col) const{
+  if(ExistsCell(row,col))
+    return vec[row*columns+col];
+  else
+    throw std::out_of_range("Out of range!");
+}
 /* ************************************************************************ */
 
 // Specific member functions (inherited from Container)
 
-void Clear() override; // Override Container member
+template <typename Data>
+void MatrixVec<Data>::Clear(){
+  vec.Clear();
+  size = 0;
+  rows = 0;
+  columns = 0;
+}
 
 /* ************************************************************************ */
 
 // Specific member functions (inherited from MappableContainer)
-
-using typename MappableContainer<Data>::MapFunctor;
-
-void MapPreOrder(const MapFunctor, void*) override; // Override MappableContainer member
-void MapPostOrder(const MapFunctor, void*) override; // Override MappableContainer member
+template <typename Data>
+void MatrixVec<Data>::MapPreOrder(const MapFunctor fun, void* par) {
+  vec.MapPreOrder(fun, par);
+}
+template <typename Data>
+void MatrixVec<Data>::MapPostOrder(const MapFunctor fun, void* par) {
+  vec.MapPostOrder(fun, par);
+}
 
 /* ************************************************************************ */
 
 // Specific member functions (inherited from FoldableContainer)
 
-using typename FoldableContainer<Data>::FoldFunctor;
+template <typename Data>
+void MatrixVec<Data>::FoldPreOrder(const FoldFunctor fun, const void* par, void* acc) const{
+  vec.FoldPreOrder(fun, par, acc);
+}
 
-void FoldPreOrder(const FoldFunctor, const void*, void*) const override; // Override FoldableContainer member
-void FoldPostOrder(const FoldFunctor, const void*, void*) const override; // Override FoldableContainer member
+template <typename Data>
+void MatrixVec<Data>::FoldPostOrder(const FoldFunctor fun, const void* par, void* acc) const{
+  vec.FoldPostOrder(fun, par, acc);
+}
 /* ************************************************************************** */
 
 }
